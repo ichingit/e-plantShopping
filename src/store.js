eb4d7f0941dc9c2e9945fd9588585e82536524cc
Sampath <ichinthaka333@gmail.com>
2024-12-27 22:03:02 -0500

    Type error commit

--- src/store.js
+++ src/store.js
@@ -1,4 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { useDispatch } from 'react-redux';
 import cartReducer from './CartSlice';
  const store = configureStore({
     reducer: {
