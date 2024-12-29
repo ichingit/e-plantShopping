1cc3c80bd2b59626c4915a3599d1e3ded30bae76
Sampath <ichinthaka333@gmail.com>
2024-12-28 20:54:36 -0500

    Cart total item commit

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -21,8 +21,12 @@ const CartItem = ({ onContinueShopping }) => {
   };
 
   const handleContinueShopping = (e) => {
-   
-  };
+           
+      };
+    
+      const handleCheckoutShopping = (e) => {
+        alert('Functionality to be added for future reference');
+      };
 
 
 
@@ -78,7 +82,7 @@ const CartItem = ({ onContinueShopping }) => {
       <div className="continue_shopping_btn">
         <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
-        <button className="get-started-button1">Checkout</button>
+        <button className="get-started-button1" onClick={(e) => handleCheckoutShopping(e)}>Checkout</button>
       </div>
     </div>
   );
